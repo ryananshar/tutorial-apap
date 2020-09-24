80bb092083af2156aac8e95008dcdc123b50fa97
ryananshar <ansharryan@gmail.com>
2020-09-24 11:24:33 +0700

    feat: tutorial 1 - initialize spring boot project

---
+++ hitungbmi/src/test/java/apap/tutorial/hitungbmi/HitungbmiApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tutorial.hitungbmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitungbmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
