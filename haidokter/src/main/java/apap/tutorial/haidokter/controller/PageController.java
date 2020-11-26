361a3533046b2caad99e5069c3c2570f2302bb21
ryananshar <ansharryan@gmail.com>
2020-11-26 12:42:52 +0700

    chore: page controller update with rolemodel usage

--- haidokter/src/main/java/apap/tutorial/haidokter/controller/PageController.java
+++ haidokter/src/main/java/apap/tutorial/haidokter/controller/PageController.java
@@ -1,12 +1,20 @@
 package apap.tutorial.haidokter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import apap.tutorial.haidokter.service.RoleService;
 
 @Controller
 public class PageController {
+    @Autowired
+    RoleService roleService;
+
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("listRole", roleService.findAll());
         return "home";
     }
 
