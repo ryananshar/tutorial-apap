f6540494404cfb73cb560c4cd44ef076c87185d7
ryananshar <ansharryan@gmail.com>
2020-11-26 17:35:42 +0700

    chore: add authorization for add obat and add user

---
+++ haidokter/src/main/java/apap/tutorial/haidokter/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package apap.tutorial.haidokter.security;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import apap.tutorial.haidokter.model.UserModel;
+import apap.tutorial.haidokter.repository.UserDb;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserDb userDb;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = userDb.findByUsername(username);
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<GrantedAuthority>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(user.getRole().getRole()));
+        return new User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+    
+}
--- haidokter/src/main/java/apap/tutorial/haidokter/security/WebSecurityConfig.java
+++ haidokter/src/main/java/apap/tutorial/haidokter/security/WebSecurityConfig.java
@@ -3,10 +3,12 @@ package apap.tutorial.haidokter.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -19,6 +21,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 authorizeRequests()
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/js/**").permitAll()
+                .antMatchers("/resep/**").hasAnyAuthority("APOTEKER")
+                // #1 Otorisasi Add User 
+                .antMatchers(HttpMethod.POST, "/**").hasAnyAuthority("ADMIN")
+                // #2 Otorisasi Add User 
+                .antMatchers("/user/addUser").hasAnyAuthority("ADMIN")
+                // Otorisasi Add Obat 
+                .antMatchers("/obat/add/**").hasAnyAuthority("APOTEKER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -33,11 +42,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         return new BCryptPasswordEncoder();
     }
 
+    // @Autowired
+    // public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    //     auth.inMemoryAuthentication()
+    //             .passwordEncoder(encoder())
+    //             .withUser("odading").password(encoder().encode("mangoleh"))
+    //             .roles("USER");
+    // }
+
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(encoder())
-                .withUser("odading").password(encoder().encode("mangoleh"))
-                .roles("USER");
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+            .passwordEncoder(encoder());
     }
 }
