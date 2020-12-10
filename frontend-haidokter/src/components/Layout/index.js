db7828df7f8123c5794a79f9d87fc0a4c8ddc8c0
ryananshar <ansharryan@gmail.com>
2020-12-10 21:13:46 +0700

    chore: add resep prop to reseplist

--- frontend-haidokter/src/components/Layout/index.js
+++ frontend-haidokter/src/components/Layout/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Layout = (props) => {
-    return
-    <main className="base-layout">
+    return (
+        <main className="base-layout">
         {props.children}
     </main>
+    )    
 };
 
 export default Layout;
--- frontend-haidokter/src/containers/ResepList/index.js
+++ frontend-haidokter/src/containers/ResepList/index.js
@@ -1,7 +1,42 @@
 import React, {Component} from "react";
 
 class ResepList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            reseps: [
+                {
+                    noResep: 1,
+                    namaDokter: "Papa APAP",
+                    namaPasien: "Anaknya Papa APAP",
+                    catatan: "Sakit ringan",
+                },
+                {
+                    noResep: 2,
+                    namaDokter: "Mama APAP",
+                    namaPasien: "Anaknya Mama APAP",
+                    catatan: "Sakit sedang",
+                },
+                {
+                    noResep: 3,
+                    namaDokter: "Kakak APAP",
+                    namaPasien: "Anaknya Kakak APAP",
+                    catatan: "Sakit parah",
+                },
+            ],
+        };
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount()");
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("shouldComponentUpdate()");
+    }
+
     render() {
+        console.log("render()");
         return (
             <div>
                 <h1>All Reseps</h1>
