1294dd0c3ea69adaea61f34edae0268b0a3b682c
ryananshar <ansharryan@gmail.com>
2020-12-10 10:48:00 +0700

    init react frontend-haidokter

---
+++ frontend-haidokter/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from "react";
+
+class App extends Component {
+  render () {
+    return <div></div> ;
+  }
+}
+
+export default App;
